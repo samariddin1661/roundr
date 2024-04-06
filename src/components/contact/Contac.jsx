9457e64ff5c56783c3e6d7e7be32350245561d21
samariddin1661 <150109553+samariddin1661@users.noreply.github.com>
2024-04-06 12:54:50 +0500

    px
    px

--- src/components/contact/Contac.jsx
+++ src/components/contact/Contac.jsx
@@ -5,7 +5,7 @@ const Contac = () => {
   return (
     <div className='w-full '>
       <div className='relative'>
-        <img src={img} alt="img" className='absolute w-[100px] right-[50%]' />
+        <img src={img} alt="img" className='absolute md:w-[100px] w-[70px] right-[50%]' />
         <TitleText title='Contacts'/>
       </div>
     </div>
