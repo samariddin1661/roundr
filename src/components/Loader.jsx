5dc71941845103306066ad7533c29eb32fdb3fee
samariddin1661 <150109553+samariddin1661@users.noreply.github.com>
2024-04-06 12:32:27 +0500

    aaa
    aaa

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div><div
+    className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-primary opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+    role="status">
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span>
+  </div></div>
   )
 }
 
