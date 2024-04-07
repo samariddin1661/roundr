4fc3825669c5558c7314ceb25c4ccd1dc49f04ce
samariddin1661 <150109553+samariddin1661@users.noreply.github.com>
2024-04-08 00:47:37 +0500

    end
    end

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className='flex justify-center items-center w-full h-[80px] bg-green-500'><marquee behavior="alternate"  > Social Media &copy; StartUp Company 2024 </marquee></div>
+      <div className='flex justify-center items-center text-[12px] md:text-[14px] w-full md:h-[80px] h-[60px] bg-green-500'><marquee behavior="alternate"  > <span className='text-[red] font-[900]'>Round Rock</span> &copy; StartUp Company 2024 </marquee></div>
     </div>
   )
 }
